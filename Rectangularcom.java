4dc26b4ff567c58b0bce8874f92be7d0d1225ad7
MD ASHIKUR RHAMAN ASHIK <ashikurrhaman724@gmail.com>
2025-10-19 17:24:48 +0600

    Java code for calculating the area of a rectangle:
    Description: Area of a Rectangle (Java Code)
    
    This Java program calculates the area and perimeter of a rectangle.
    
    Input:
    
    The program asks the user to enter the width and height of the rectangle using the Scanner class.
    
    Processing:
    
    The area is calculated using the formula:
    
    Area
    =
    Width
    ×
    Height
    Area=Width×Height
    
    The perimeter is calculated using the formula:
    
    Perimeter
    =
    2
    ×
    (
    Width
    +
    Height
    )
    Perimeter=2×(Width+Height)
    
    Output:
    
    The program prints the width, height, area, and perimeter on the console.
    
    The output is formatted to show 2 decimal places using System.out.printf().
    
    Purpose / Objective:
    
    Learn to take user input in Java.
    
    Perform basic arithmetic operations.
    
    Display results in a formatted manner.
    
    Understand practical application of formulas in programming.

---
+++ Rectangularcom.java
@@ -0,0 +1,21 @@
+class Rectcom{
+	public static void main(String[] abc){
+		String Width= abc[0];
+		String height = abc[1];
+		double width= Double.parseDouble(Width);
+		double Height = Double.parseDouble(height);
+
+	double area = width*Height;
+	double perimeter = 2*(width+Height);
+
+		System.out.println("Width " + " = "+width);
+		System.out.println("Heght " + " = "+Height);
+		System.out.printf("Area = %.2f\n",area);
+		System.out.printf("Perimeter %.2f\n",perimeter);
+		
+
+}	
+
+}
+
+	
